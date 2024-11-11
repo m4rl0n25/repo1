c011fe53161158c981230b5974af67b4a2eb7888
Dillon Lyons <dillonlyons@protonmail.com>
2024-11-11 16:33:16 -0600

    Reverting hard coded secret

--- Sensitive Data Exposure/Railsgoatconfig.rb
+++ Sensitive Data Exposure/Railsgoatconfig.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Railsgoat::Application.config.secret_key_base = "aefkhaeufvbvjbfndlkba"
+Railsgoat::Application.config.secret_key_base = "aefkhaeufvbvjbfñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqya"
