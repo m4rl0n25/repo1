4114add45a3808cb2f699314c2251d698c9a7901
Dillon Lyons <dillonlyons@protonmail.com>
2024-11-11 16:34:04 -0600

    updating hard coded secret

--- Sensitive Data Exposure/Railsgoatconfig.rb
+++ Sensitive Data Exposure/Railsgoatconfig.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Railsgoat::Application.config.secret_key_base = "aefkhaeufvbvjbfñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqya"
+Railsgoat::Application.config.secret_key_base = "ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy"
